a6d766469704f2041621843097e217f01539152b
Piotr Żuralski <piotr.zuralski@gmail.com>
2019-03-24 11:06:25 +0100

    zmiany

--- Mapper/Mapper.cpp
+++ Mapper/Mapper.cpp
@@ -8,48 +8,63 @@
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
-	std::map<int, double> mapa;
+class Mapper {
 
-	mapa[1] = 10;
-	mapa[4] = 30;
-	mapa[3] = 50;
-	mapa[2] = 70;
-	
-	cout << mapa[2];
-	cout << endl;
+	private: 
+		std::map<int, double> mapa;
 
-	mapa.insert(std::pair<int, double>(2, 100));
-
-	//std::map<char, int> second(first.begin(), first.end());
-	//std::map<char, int> third(second);
-
-	cout << mapa.size();
-	cout << endl;
-
-	std::map<int, double>::iterator item = mapa.begin();
-	cout << "mapa contains:\n";
-	for (item = mapa.begin(); item != mapa.end(); ++item)
+	public: Mapper() {
+		
+	}
+	public: int init()
 	{
-		cout << item->first << " => " << item->second << endl;
+		mapa[1] = 10;
+		mapa[4] = 30;
+		mapa[3] = 50;
+		mapa[2] = 70;
+
+		cout << mapa[2];
+		cout << endl;
+	}
+	
+	public: string show()
+	{
+		string result = "";
+		std::map<int, double>::iterator item = mapa.begin();
+		for (item = mapa.begin(); item != mapa.end(); ++item)
+		{
+			result += item->first + " => " + item->second + endl;
+		}
+		return result;
 	}
 
-	mapa.clear();
-	cout << (mapa.size() == 0) << endl;
+	public: void clear()
+	{
+		mapa.clear();
+	}
 
-	//Przygotowaæ testy - testom poddaæ nastêpuj¹ce metody :
-	//	• pair<iterator, bool> insert(const value_type& val).
-	//	• mapped_type& operator[](const key_type& k).
-	//	• iterator find(const key_type& k).
-	//	• size_type size() const.
-	//	• size_type erase(const key_type& k).
-	//	• void clear().
-	//	• iterator begin(), iterator end() - iterowaæ po kontenerze
+	public: int main()
+	{
+		// mapa.insert(std::pair<int, double>(2, 100));
 
-	system("pause");
-	
-	return 0;
-	
-}
+		//std::map<char, int> second(first.begin(), first.end());
+		//std::map<char, int> third(second);
 
+		cout << mapa.size();
+		cout << endl;
+
+		cout << (mapa.size() == 0) << endl;
+
+		//Przygotowaæ testy - testom poddaæ nastêpuj¹ce metody :
+		//	• pair<iterator, bool> insert(const value_type& val).
+		//	• mapped_type& operator[](const key_type& k).
+		//	• iterator find(const key_type& k).
+		//	• size_type size() const.
+		//	• size_type erase(const key_type& k).
+		//	• void clear().
+		//	• iterator begin(), iterator end() - iterowaæ po kontenerze
+		system("pause");
+		return 0;
+	}
+
+};
--- MapperTest/unittest1.cpp
+++ MapperTest/unittest1.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "../Mapper/resource.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
