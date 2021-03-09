e801763f0c1351edbd9e97e8df77d1df376f2a9a
Piotr Żuralski <piotr.zuralski@gmail.com>
2021-03-09 20:20:57 +0100

    commit on 2021-03-09T19:20:57+00:00

--- ININ6_PR1.1-Zaawansowanie-programowanie-w-jezyku-Python/Lab_1-Project_1/Case_4.py
+++ ININ6_PR1.1-Zaawansowanie-programowanie-w-jezyku-Python/Lab_1-Project_1/Case_4.py
@@ -6,3 +6,14 @@ przedziale).
 • Proszę pamiętać że b może być większe od a lub odwrotnie a może być
 większe od b.
 """
+a = int(input("Podaj liczbę a: "))
+b = int(input("Podaj liczbę b: "))
+
+start = a
+end = b
+if end < start:
+    start = b
+    end = a
+
+liczby = range(start, end+1, 1)
+print("Liczby z przedziału <{0}; {1}>: {2:s}.".format(start, end, ", ".join(["{0:d}".format(l) for l in liczby])))
--- ININ6_PR1.1-Zaawansowanie-programowanie-w-jezyku-Python/Lab_1-Project_1/Case_5.py
+++ ININ6_PR1.1-Zaawansowanie-programowanie-w-jezyku-Python/Lab_1-Project_1/Case_5.py
@@ -4,3 +4,23 @@
 Fibonacciego dla podanego n.
 • Pobrać n od użytkownika i wypisać na ekran fib(n).
 """
+
+
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+while True:
+    try:
+        n = int(input("Podaj liczbę n: "))
+    except ValueError:
+        n = 0
+
+    if n <= 0:
+       print("Podaj liczbę całkowitą wiekszą od zera")
+    else:
+        print(fib(n))
+        break
