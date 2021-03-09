7063ecb25df980c17901e167f71d8a9fa7945a50
Piotr Żuralski <piotr.zuralski@gmail.com>
2021-03-09 18:19:31 +0100

    commit on 2021-03-09T17:19:31+00:00

--- ININ6_PR1.1-Zaawansowanie-programowanie-w-jezyku-Python/Lab_1-Project_1/Case_3.py
+++ ININ6_PR1.1-Zaawansowanie-programowanie-w-jezyku-Python/Lab_1-Project_1/Case_3.py
@@ -5,3 +5,11 @@
 • Wypisz na ekran wyrazy w kolejności odwrotnej niż pobierana (ostatni jako
 pierwszy). Nie wypisuj słowa "koniec".
 """
+lista = []
+while True:
+    wyraz = input('Podaj wyraz do dodania (aby zakończyć, podaj "koniec"): ')
+    if wyraz == 'koniec':
+        break
+    lista.append(wyraz)
+
+print("Podane wyrazy w odwrotnej kolejności: {0:s}".format(", ".join(reversed(lista))))
