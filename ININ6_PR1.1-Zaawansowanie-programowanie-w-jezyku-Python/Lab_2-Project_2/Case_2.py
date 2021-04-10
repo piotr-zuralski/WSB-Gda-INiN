caa97e346764f0d1c5d9c9d632e8e57698e8d093
Piotr Żuralski <piotr.zuralski@gmail.com>
2021-04-10 20:45:28 +0200

    commit on 2021-04-10T18:45:28+00:00

--- ININ6_PR1.1-Zaawansowanie-programowanie-w-jezyku-Python/Lab_2-Project_2/Case_2.py
+++ ININ6_PR1.1-Zaawansowanie-programowanie-w-jezyku-Python/Lab_2-Project_2/Case_2.py
@@ -2,6 +2,28 @@
 # -*- coding: utf-8 -*-
 
 """
-
+2. [1 punkt] Numer do cioci z ameryki
+Na wejście zostanie podana lista dziesięciu cyfr. Należy z niej
+złożyć numer w formacie:
+(XXX) XXX-XXXX
+I wypisać na ekran.
+Przykładowe wejście:
+1 2 3 4 5 6 7 8 9 0
+Przykładowe wyjście:
+(123) 456-7890
 """
 
+
+def podaj_cyfry():
+    _cyfry = input('Podaj listę 10 cyfr: ').replace(' ', '')
+    _cyfry = list(filter(lambda x: int(float(x)) or x.isdigit(), _cyfry))
+    return _cyfry
+
+
+cyfry = podaj_cyfry()
+while not len(cyfry) == 10:
+    print("Nieprawidłowa ilość cyfr")
+    cyfry = podaj_cyfry()
+
+cyfry = ''.join(cyfry)
+print("({0}) {1}-{2}".format(cyfry[0:3], cyfry[3:6], cyfry[6:10]))
