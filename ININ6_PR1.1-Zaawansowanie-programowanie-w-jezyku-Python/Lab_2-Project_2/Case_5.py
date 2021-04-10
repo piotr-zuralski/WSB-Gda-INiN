c6dcb979dd3280695aede264aeb2d1508e4e6dae
Piotr Żuralski <piotr.zuralski@gmail.com>
2021-04-10 21:59:46 +0200

    commit on 2021-04-10T19:59:46+00:00

--- ININ6_PR1.1-Zaawansowanie-programowanie-w-jezyku-Python/Lab_2-Project_2/Case_5.py
+++ ININ6_PR1.1-Zaawansowanie-programowanie-w-jezyku-Python/Lab_2-Project_2/Case_5.py
@@ -2,6 +2,30 @@
 # -*- coding: utf-8 -*-
 
 """
-
+[3 punkty] Zliczanie wyrazów
+Na wejście zostanie podana seria zdań. Ostatnie zdanie będzie zakończone
+słowem KONIEC, wtedy należy przerwać wczytywanie. Należy napisać
+program, który zliczy ilość wystąpień każdego wyrazu, a następnie wypisze
+zestawienie na ekran, ale tylko dla wyrazów, które pojawiły się częściej niż
+raz.
+Proszę przy wczytywaniu usunąć znaki diakrytyczne takie jak przecinek i
+kropka.
+Przykładowe wejście:
+Podstawowym obowiązkiem intelektualisty jest myśleć i mówić
+prawdę.
+Jeśli masz dwie drogi do wyboru, wybieraj zawsze drogę
+trudniejszą dla ciebie.
+bądź odważny, gdy rozum zawodzi, bądź odważny
+w ostatecznym rachunku jedynie to się liczy.
+idź wyprostowany wśród tych co na kolanach
+Każda opowieść Herberta zawiera ukryty sens. (…) Herbert daje
+nam takich bogów, na jakich zasłużyliśmy.
+odważny, wyprostowany, prawdę, zasłużymy. KONIEC.
+Przykładowe wyjście:
+prawdę : 2
+bądź : 2
+odważny : 3
+wyprostowany : 2
+na : 2
 """
 
